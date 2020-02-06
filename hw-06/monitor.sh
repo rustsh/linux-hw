e9d8ee7d5098cfa506ff76905bd72add7010b37b
Rustam Shaimardanov <44144831+rustsh@users.noreply.github.com>
2020-02-06 21:42:09 +0000

    HW-06 Make script a little bit prettier

--- hw-06/monitor.sh
+++ hw-06/monitor.sh
@@ -34,7 +34,7 @@ then
     topList() {
         echo "$1" | awk '{ ipcount[$1]++ } END { for (i in ipcount) \
                          { if (ipcount[i] ~ /^[2-4]$|[^1][2-4]$/) case="раза"; else case="раз"; \
-                         printf "%s - %d %s\n", i, ipcount[i],case } }' | sort -rnk3 -k1 | head -$2
+                         printf "%s — %d %s\n", i, ipcount[i], case } }' | sort -rnk3 -k1 | head -$2
     }
 
     ### Считываем время последнего запуска скрипта из файла лога
@@ -52,31 +52,31 @@ then
 
     ### Выводим временной диапазон
     echo "Данные за промежуток между $lastTime и $timeNow" > $outputFile
-    echo '' >> $outputFile
+    echo >> $outputFile
 
     ### Выводим IP-адреса с наибольшим количеством запросов
     echo "Топ $countFrom IP-адресов:" >> $outputFile
     topList "$logInterval" $countFrom >> $outputFile
-    echo '' >> $outputFile
+    echo >> $outputFile
 
     ### Выводим запрашиваемые адреса с наибольшим количеством запросов
     echo "Топ $countTo запрашиваемых адресов:" >> $outputFile
     # Выводим адрес из запроса, если этот запрос начинается с указания метода (GET, POST и т. д.), иначе выводим прочерк:
     addrList=$(echo "$logInterval" | awk '{ if ($6 ~ /^"[A-Z]+$/) print $7; else print "-" }')
     topList "$addrList" $countTo >> $outputFile
-    echo '' >> $outputFile
+    echo >> $outputFile
 
     ### Запоминаем все коды возврата
     codeList=$(echo "$logInterval" | awk '{ if ($9 ~ /^[0-9]+$/) print $9; else print $7 }')
 
     ### Выводим все ошибки
-    echo 'Присутствующие ошибки:' >> $outputFile
+    echo "Присутствующие ошибки:" >> $outputFile
     # Отсортируем все коды возврата, оставим только уникальные значения и при помощи sed удалим коды 2xx и 3xx:
     echo "$codeList" | sort -u | sed '/^[23]/d' >> $outputFile
-    echo '' >> $outputFile
+    echo >> $outputFile
 
     ### Выводим список всех кодов возврата с указанием их количества
-    echo 'Коды возврата:' >> $outputFile
+    echo "Коды возврата:" >> $outputFile
     # Посчитаем число уникальных кодов, чтобы передать его как параметр команды head в функции topList:
     codeCount=$(echo "$codeList" | sort -u | wc -l)
     topList "$codeList" $codeCount >> $outputFile
@@ -96,6 +96,6 @@ then
     rm -f "$lockfile"
     trap - INT TERM EXIT
 else
-    echo "Failed to acquire lockfile: $lockfile."
+    echo "Failed to acquire lockfile: $lockfile"
     echo "Held by $(cat $lockfile)"
 fi
